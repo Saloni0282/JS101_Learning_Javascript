dcfa2e97d256bd752dc62791c13d0759ef9c971b
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2022-11-02 12:44:42 +0530

    First commit

---
+++ JS101_Learning_Javascript/01.js
@@ -0,0 +1,7 @@
+
+let name = "Saloni_Kumari";
+console.log(name);
+let fatherName = "Naresh_Rajak";
+console.log(fatherName);
+let motherName = "Renu_Kumari";
+console.log(motherName);
---
+++ JS101_Learning_Javascript/03.js
@@ -0,0 +1,23 @@
+
+
+let name = "Harshit";
+let school = "Prabhat_Tara_School";
+let grade = "A"
+let section = "A";
+let rollNo = "75";
+console.log(`Name = ${name} School = ${school} `);
+console.log();
+console.log(`Rollno.= ${rollNo} Section = ${section}`);
+console.log(" ");
+let eng = 82;
+let hin = 80;
+let mat = 85;
+console.log("English = " + (eng) + "/" + 100);
+console.log(" ");
+console.log("Hindi  =  " + (hin) + "/" + 100);
+console.log(" ");
+console.log("Math   = " + (mat) + "/" + 100);
+console.log(" ");
+console.log("Total  = " + (eng + hin + mat) + "/" + 300);
+console.log(" ");
+console.log("Grade = " + grade);
